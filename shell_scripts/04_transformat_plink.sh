e67efe2a641de7ebff75c928e1b3c5e2ece3ef34
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-11-07 10:48:25 -0500

    minor fixes for 04 script

--- shell_scripts/04_transformat_plink.sh
+++ shell_scripts/04_transformat_plink.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=5
 
-#SBATCH --array=1-21
+#SBATCH --array=1-22
 
 bash
 let k=0
@@ -16,7 +16,7 @@ sample=/net/mulan/disk2/yasheng/predictionProject/plink_file/pheno_list/ukb_all_
 brIdx1=/net/mulan/disk2/yasheng/predictionProject/plink_file/pheno_list/brIdx1.txt
 brIdx2=/net/mulan/disk2/yasheng/predictionProject/plink_file/pheno_list/brIdx2.txt
 #chr=20
-for ((chr=1;chr<22;chr++));do
+for ((chr=1;chr<23;chr++));do
 let k=${k}+1
 if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
 bgen=/net/mulan/Biobank/rawdata/EGAD00010001225/001/ukb_imp_chr${chr}_v2.bgen
@@ -34,7 +34,7 @@ rm ${bfile1}.bim
  fi
  done
 
-SEND_THREAD_NUM=6
+SEND_THREAD_NUM=21
 tmp_fifofile="/tmp/$$.fifo"
 mkfifo "$tmp_fifofile"
 exec 6<>"$tmp_fifofile"
@@ -43,17 +43,12 @@ for ((i=0;i<$SEND_THREAD_NUM;i++));do
 echo
 done >&6
 
-for chr in `seq 1 6`;do
+for chr in `seq 1 22`;do
 read -u6
 {
-bfile1=~/research/ukb-intervals/dat/plink_files/genotype/chr${chr}
 bfile2=~/research/ukb-intervals/dat/plink_files/ukb/chr${chr}
-removeid=/net/mulan/disk2/yasheng/comparisonProject/w30186_idx.txt
-plink-1.9 --bfile ${bfile1} --maf 0.01 --remove ${removeid} --make-bed --out ${bfile2}
-rm ${bfile1}*
-bfile3=~/research/ukb-intervals/dat/plink_files/hm3/chr${chr}
-snplist=~/research/ukb-intervals/dat/plink_files/snplist.txt
-plink-1.9 --bfile ${bfile2} --maf 0.01 --extract ${snplist} --make-bed --out ${bfile3}
+bfile3=~/research/ukb-intervals/dat/plink_files/low/chr${chr}
+plink-1.9 --bfile ${bfile2} --maf 0.001 --make-bed --out ${bfile3}
 #rm ${bfile2}.log
 #rm ${bfile3}.log
 } &
