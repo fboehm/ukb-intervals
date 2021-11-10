6bb620142a605b2a55d2593888f9cd8d9ceaeab0
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-11-10 10:11:07 -0500

    added ldsc code for *my traits* ie my fam files!

--- shell_scripts/09_make_reference_ldsc.sh
+++ shell_scripts/09_make_reference_ldsc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SEND_THREAD_NUM=21
+SEND_THREAD_NUM=22
 tmp_fifofile="/tmp/$$.fifo"
 mkfifo "$tmp_fifofile"
 exec 6<>"$tmp_fifofile"
@@ -9,15 +9,15 @@ for ((i=0;i<$SEND_THREAD_NUM;i++));do
 echo
 done >&6
 
-for chr in `seq 1 21`;do
+for chr in `seq 1 22`;do
 read -u6
 {
 ldsc=/net/mulan/home/yasheng/comparisonProject/program/ldsc/ldsc.py
 cd /net/mulan/home/yasheng/comparisonProject/program/ldsc
 source activate ldsc
 
-${ldsc} --out /net/mulan/disk2/yasheng/comparisonProject/03_subsample/ukb/ldsc/${chr} \
-		--bfile /net/mulan/disk2/yasheng/comparisonProject/03_subsample/ukb/geno/chr${chr} \
+${ldsc} --out ~/research/ukb-intervals/dat/04_reference/ukb/ldsc/${chr} \
+		--bfile ~/research/ukb-intervals/dat/03_subsample/ukb/geno/chr${chr} \
 		--l2  --ld-wind-kb 1000.0 
 } &
 pid=$!
