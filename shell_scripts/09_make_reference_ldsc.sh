19dcd64c741d8f3b8587c724008e9358796b7672
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-11-29 14:03:55 -0500

    using only one chr

--- shell_scripts/09_make_reference_ldsc.sh
+++ shell_scripts/09_make_reference_ldsc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SEND_THREAD_NUM=22
+#SEND_THREAD_NUM=22
+SEND_THREAD_NUM=1
 tmp_fifofile="/tmp/$$.fifo"
 mkfifo "$tmp_fifofile"
 exec 6<>"$tmp_fifofile"
@@ -9,7 +10,8 @@ for ((i=0;i<$SEND_THREAD_NUM;i++));do
 echo
 done >&6
 
-for chr in `seq 1 22`;do
+#for chr in `seq 1 22`;do
+for chr in 1; do
 read -u6
 {
 ldsc=/usr/cluster/ldsc/ldsc.py
