56626a2e471debc8dbff8ff6f797584a2c713e3a
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-12-05 21:04:44 -0500

    fixed naming of test and training sets

--- Rscript/sample-subjects.R
+++ Rscript/sample-subjects.R
@@ -6,9 +6,9 @@ fam <- readr::read_delim("~/research/ukb-intervals/dat/plink_files/ukb/chr1.fam"
 set.seed(2021-12-05)
 ss <- sample(1:nrow(fam), size = 10000)
 tr <- fam
-tr[!(1:nrow(fam) %in% ss), 6:ncol(fam)] <- NA
+tr[(1:nrow(fam) %in% ss), 6:ncol(fam)] <- NA
 te <- fam
-te[(1:nrow(fam) %in% ss), 6:ncol(fam)] <- NA
+te[!(1:nrow(fam) %in% ss), 6:ncol(fam)] <- NA
 
 readr::write_delim(x = tr, file = "~/research/ukb-intervals/dat/plink_files/ukb/chr1-training.fam")
 readr::write_delim(x = te, file = "~/research/ukb-intervals/dat/plink_files/ukb/chr1-test.fam")
