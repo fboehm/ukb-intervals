8038fdb7cb82a76d7f2c9b27ba5efaf91609cbe2
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-12-05 20:08:17 -0500

    simplified code

--- shell_scripts/06_DBSLMM_ukb.sh
+++ shell_scripts/06_DBSLMM_ukb.sh
@@ -21,19 +21,10 @@ thread=5
 
 dat=c
 type=d
-if [[ "$dat" == "c" ]]
-then
-#phenoVal=/net/mulan/disk2/yasheng/comparisonProject/03_subsample/02_pheno_c.txt
 phenoVal=~/research/ukb-intervals/dat/03_subsample/02_pheno_c.txt
 index=r2
-#else
-#phenoVal=/net/mulan/disk2/yasheng/comparisonProject/03_subsample/03_pheno_b.txt
-#index=auc
-fi
 plink=/usr/cluster/bin/plink-1.9
-#DBSLMM=/net/mulan/disk2/yasheng/comparisonProject/code/02_method/06_DBSLMM_script.sh
 DBSLMM=~/research/ukb-intervals/shell_scripts/06_DBSLMM_script.sh
-#DBSLMMpath=/net/mulan/home/yasheng/predictionProject/code/
 DBSLMMpath=~/research/
 blockf=/net/mulan/disk2/yasheng/comparisonProject/LDblock_EUR/chr
 ref=/net/mulan/disk2/yasheng/comparisonProject-archive/03_subsample/ukb/geno/chr
@@ -41,66 +32,31 @@ ref=/net/mulan/disk2/yasheng/comparisonProject-archive/03_subsample/ukb/geno/chr
 phenoMiss=/net/mulan/disk2/yasheng/comparisonProject/code/02_method/DBSLMM_miss/pheno.txt
 crossMiss=/net/mulan/disk2/yasheng/comparisonProject/code/02_method/DBSLMM_miss/cross.txt
 
-for p in 1 # pheno number
-do
-for cross in 5 # look only at fold 5
-do
+let p=1 # pheno number
 let k=${k}+1
 if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
+  
+  ## input
+  herit=/net/mulan/disk2/yasheng/comparisonProject/04_reference/hm3/ldsc/1.log
+  summ=~/research/ukb-intervals/shell_scripts/output/summary_ukb_pheno${p}_training_chr
+  outPath=~/research/ukb-intervals/dat/05_internal_c/pheno${p}/ukb/
+  if ((${p}==1 || ${p}==6 || ${p}==21))
+  then
+    cov=/net/mulan/disk2/yasheng/comparisonProject/03_subsample/05_cov_nosex.txt
+    else
+    cov=/net/mulan/disk2/yasheng/comparisonProject/03_subsample/04_cov.txt
+  fi
+  
+  
+  
+  ## DBSLMM
+  esttime=~/research/ukb-intervals/dat/01_time_file/06_DBSLMM_ukb_${dat}_pheno${p}_training_thread${thread}.tm
 
-## input
-if [[ "$dat" == "c" ]]
-then
-#herit=/net/mulan/disk2/yasheng/comparisonProject/05_internal_c/pheno${p}/herit/h2_ukb_cross${cross}.log
-herit=/net/mulan/disk2/yasheng/comparisonProject/04_reference/hm3/ldsc/1.log
-#summ=/net/mulan/disk2/yasheng/comparisonProject/05_internal_c/pheno${p}/output/summary_ukb_cross${cross}_chr
-#summ=~/research/ukb-intervals/dat/05_internal_c/pheno${p}/output/summary_ukb_pheno${p}_cross${cross}_chr
-summ=~/research/ukb-intervals/shell_scripts/output/summary_ukb_pheno${p}_cross${cross}_chr
-outPath=~/research/ukb-intervals/dat/05_internal_c/pheno${p}/ukb/
-#else
-#herit=/net/mulan/disk2/yasheng/comparisonProject/06_internal_b/pheno${p}/herit/h2_ukb_cross${cross}.log
-#summ=/net/mulan/disk2/yasheng/comparisonProject/06_internal_b/pheno${p}/output/summary_ukb_cross${cross}_chr
-#summ=~/research/ukb-intervals/shell_scripts/output/summary_ukb_pheno${p}_cross${cross}_chr
-#outPath=/net/mulan/disk2/yasheng/comparisonProject/06_internal_b/pheno${p}/DBSLMM/ukb/
-if ((${p}==1 || ${p}==6 || ${p}==21))
-then
-cov=/net/mulan/disk2/yasheng/comparisonProject/03_subsample/05_cov_nosex.txt
-else
-cov=/net/mulan/disk2/yasheng/comparisonProject/03_subsample/04_cov.txt
-fi
-fi
+  time /usr/bin/time -v -o ${esttime} 
+  sh ${DBSLMM} -D ${DBSLMMpath} -p ${plink} -B ${blockf} -s ${summ} -m DBSLMM\
+               -H ${herit} -G ${ref} -P ${phenoVal}\
+               -l ${p} -T ${type} -i ${index} -t ${thread} -training true -o ${outPath}
 
-#for chr in `seq 1 22`
-#for chr in 1
-#do
-#gunzip ${summ}${chr}.assoc.txt.gz
-#done
+fi # end if statement that matches array task
 
 
-
-## DBSLMM
-#esttime=/net/mulan/disk2/yasheng/comparisonProject/01_time_file/06_DBSLMM_ukb_${dat}_pheno${p}_cross${cross}_thread${thread}.tm
-esttime=~/research/ukb-intervals/dat/01_time_file/06_DBSLMM_ukb_${dat}_pheno${p}_cross${cross}_thread${thread}.tm
-if [[ "$dat" == "c" ]]
-then
-
-time /usr/bin/time -v -o ${esttime} 
-sh ${DBSLMM} -D ${DBSLMMpath} -p ${plink} -B ${blockf} -s ${summ} -m DBSLMM\
-             -H ${herit} -G ${ref} -P ${phenoVal}\
-             -l ${p} -T ${type} -i ${index} -t ${thread} -o ${outPath}
-#else
-#time /usr/bin/time -v -o ${esttime} sh ${DBSLMM} -D ${DBSLMMpath} -p ${plink} -B ${blockf} -s ${summ} -m DBSLMM\
-#                                                 -H ${herit} -G ${ref} -P ${phenoVal}\
-#                                                 -l ${p} -T ${type} -c ${cov} -i ${index} -t ${thread} -o ${outPath}
-
-fi
-
-#for chr in `seq 1 22`
-#for chr in 1
-#do
-#gzip ${summ}${chr}.assoc.txt
-#done
-
-fi
-done
-done
