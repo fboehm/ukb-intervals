b0d8a2b743d2c22b1c25b9dbfce867881a0530b8
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-12-13 15:18:37 -0500

    fixed subsetting text file

--- Rscript/sample_subjects.R
+++ Rscript/sample_subjects.R
@@ -6,7 +6,7 @@
 fam <- readr::read_delim("~/research/ukb-intervals/dat/plink_files/ukb/chr1.fam", col_names = FALSE)
 set.seed(2021-12-05)
 ss <- sample(1:nrow(fam), size = 10000)
-readr::write_delim(x = tibble::as_tibble(ss), file = "~/research/ukb-intervals/dat/test-subjects-ids.txt", col_names = FALSE)
+readr::write_delim(x = tibble::as_tibble(fam$X1[ss]), file = "~/research/ukb-intervals/dat/test-subjects-ids.txt", col_names = FALSE)
 tr <- fam
 tr[(1:nrow(fam) %in% ss), 6:ncol(fam)] <- NA
 te <- fam
