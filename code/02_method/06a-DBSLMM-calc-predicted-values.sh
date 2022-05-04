899184cca868bbd2690ac6b046968dfc39a19ac5
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-05-04 11:53:17 -0400

    fixed file path for outputs of predict

--- code/02_method/06a-DBSLMM-calc-predicted-values.sh
+++ code/02_method/06a-DBSLMM-calc-predicted-values.sh
@@ -26,9 +26,9 @@ bfile=/net/mulan/disk2/yasheng/predictionProject/plink_file/ukb/chr${chr}
 idxtest=~/research/comparisonProject/02_pheno/01_test_idx_c/idx_pheno${p}_cross${cross}.txt
 #idxtest=~/research/ukb-intervals/test_index_files/test_indices_pheno_${p}_cross_${cross}_ntest1000.txt
 #esteffdbslmmt=${compstr}05_internal_c/pheno${p}/DBSLMM/summary_hm3_cross${cross}_chr${chr}_best.dbslmm.txt
-esteffdbslmmt=~/research/ukb-intervals/results/pheno1/summary_ukb_cross${cross}_chr${chr}_auto.dbslmm.txt
+esteffdbslmmt=~/research/ukb-intervals/results/pheno${p}/summary_ukb_cross${cross}_chr${chr}_auto.dbslmm.txt
 #preddbslmmt=${compstr}05_internal_c/pheno${p}/DBSLMM/pred_hm3_best_cross${cross}_chr${chr}
-preddbslmmt=~/research/ukb-intervals/results/pheno1/predicted_ukb_cross${cross}_chr${chr}_auto.dbslmm.txt
+preddbslmmt=~/research/ukb-intervals/results/pheno${p}/predicted_ukb_cross${cross}_chr${chr}_auto.dbslmm.txt
 # aggdbslmmt=${compstr}05_internal_c/pheno${p}/DBSLMM/agg_hm3_best_cross${cross}_chr${chr}
 #gunzip ${esteffdbslmmt}.gz
 time /usr/bin/time -v -o ~/research/ukb-intervals/cluster_outputs/time-prediction-dbslmm-pheno${p}-cross${cross}-chr${chr}.txt plink-1.9 --silent --bfile ${bfile} --score ${esteffdbslmmt} 1 2 4 sum --keep ${idxtest} --out ${preddbslmmt}
