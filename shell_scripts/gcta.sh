e8f9b78b3966fed6bb07e61cfcbd73b4f5d2741e
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-06-07 20:13:30 -0400

    minor updates to scripts

--- shell_scripts/gcta.sh
+++ shell_scripts/gcta.sh
@@ -12,4 +12,8 @@
 #SBATCH --mail-user=fredboe@umich.edu  
 #SBATCH --mail-type=ALL
 
-Rscript -e "rmarkdown::render('~/research/ukb-intervals/Rmd/simulating-traits-with-GCTA-chr22.Rmd')"
+gcta64 --bfile ../dat/simulations-ding/chr22 --simu-qt --simu-causal-loci ../dat/simulations-ding/snp_effects_Chr22_hsq0.2_pcausal0.1.txt --simu-hsq 0.2 --simu-rep 10 --out ../dat/simulations-ding/sim_traits/sims_Chr22_hsq0.2_pcausal0.1
+
+gcta64 --bfile ../dat/simulations-ding/chr22 --simu-qt --simu-causal-loci ../dat/simulations-ding/snp_effects_Chr22_hsq0.1_pcausal0.1.txt --simu-hsq 0.1 --simu-rep 10 --out ../dat/simulations/sim_traits/sims_Chr22_hsq0.1_pcausal0.1
+
+gcta64 --bfile ../dat/simulations-ding/chr22 --simu-qt --simu-causal-loci ../dat/simulations-ding/snp_effects_Chr22_hsq0.5_pcausal0.1.txt --simu-hsq 0.5 --simu-rep 10 --out ../dat/simulations/sim_traits/sims_Chr22_hsq0.5_pcausal0.1
