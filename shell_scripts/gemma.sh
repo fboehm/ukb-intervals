e5733d17775f9602ec7a9d6671d264d661f35e4c
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-06-14 10:50:19 -0400

    renamed shell script and added bash chunk to Rmd

--- shell_scripts/gemma.sh
+++ shell_scripts/gemma.sh
@@ -2,13 +2,13 @@
 
 
 #SBATCH --partition=mulan,nomosix
-#SBATCH --time=4:00:00
-#SBATCH --job-name=gemma-for-ldsc
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=gemma
 #SBATCH --mem=2G
 #SBATCH --cpus-per-task=1
-#SBATCH --array=1-220%20
-#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/gemma-for-ldsc_%a.out
-#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/gemma-for-ldsc_%a.err
+#SBATCH --array=1-1100%50
+#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/gemma_%a.out
+#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/gemma_%a.err
 #SBATCH --mail-user=fredboe@umich.edu  
 #SBATCH --mail-type=ALL
 
@@ -20,6 +20,8 @@ gemma=/net/mulan/home/yasheng/comparisonProject/program/gemma-0.98.1-linux-stati
 
 for p in `seq 1 10`
 do
+for fold in `seq 1 5`
+do 
 for chr in `seq 1 22`
 do
 
@@ -27,13 +29,14 @@ let k=${k}+1
 if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]
 then
 
-bfile=~/research/ukb-intervals/dat/simulations-ding/gemma-for-ldsc/chr${chr}
-summ=summary_${type}_chr${chr}_pheno${p}_for-ldsc
+bfile=~/research/ukb-intervals/dat/simulations-ding/gemma/chr${chr}
+summ=summary_${type}_chr${chr}_pheno${p}_fold${fold}
 
-col=${p}
+col=(${p} - 1) * 5 + ${fold}
 ${gemma} -bfile ${bfile} -notsnp -lm 1 -n ${col} -o ${summ}
 
 fi
 
 done 
+done
 done
