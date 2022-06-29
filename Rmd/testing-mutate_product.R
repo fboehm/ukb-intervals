333a831e750fe5ee99d3df669537ec716fb9e5cd
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-06-29 11:58:55 -0400

    fixed mutate_product function code

--- Rmd/testing-mutate_product.R
+++ Rmd/testing-mutate_product.R
@@ -1,3 +1,23 @@
+library(magrittr)
+
+mutate_product <- function(data, trait_num, fold_num){
+  for (trait in seq_along(trait_num)){
+    for (fold in seq_along(fold_num)){
+      trait_col_indic <- paste0("fold", fold, "_na")
+      trait_col <- paste0("V", trait)
+      data <- data %>%
+        dplyr::mutate(
+          "tr{{trait}}_fold{{fold}}" := .data[[trait_col]] * tr_indic[[trait_col_indic]]
+        ) %>%
+        dplyr::rename_with(.fn = function(x)stringr::str_remove_all(x, pattern = "L"))
+    }
+  }
+  return(data)
+}
+
+
+
+
 tr_indic <- tibble::tibble(fold1_na = c(NA, 1, NA))
 
 tibble::tibble(V1 = c(1,2,3), V2 = c(4:6)) %>%
