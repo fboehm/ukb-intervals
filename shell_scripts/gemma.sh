58bca65b909e2c6564cfa5584208e1b93506e150
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-07-21 10:06:14 -0400

    switched to 75 jobs at a time

--- shell_scripts/gemma.sh
+++ shell_scripts/gemma.sh
@@ -6,24 +6,14 @@
 #SBATCH --job-name=gemma
 #SBATCH --mem=2G
 #SBATCH --cpus-per-task=1
-#SBATCH --array=1-1100%200
-#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/gemma_hsq0.1_pcausal0.1/gemma_%a.out
-#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/gemma_hsq0.1_pcausal0.1/gemma_%a.err
+#SBATCH --array=1-1100%75
+#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/gemma_hsq0.5_pcausal0.1/gemma_%a.out
+#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/gemma_hsq0.5_pcausal0.1/gemma_%a.err
 
 
-# parse command line args
-# https://www.baeldung.com/linux/use-command-line-arguments-in-bash-script
-# https://help.rc.ufl.edu/doc/Using_Variables_in_SLURM_Jobs
-#while getopts h:p: flag
-#do
-#    case "${flag}" in
-#        h) hsq=${OPTARG};;
-#        p) pc=${OPTARG};;
-#    esac
-#done
 # h and p are passed as command line args
-hsq=${h}
-pc=${p}
+hsq=0.5
+pc=0.1
 
 
 # 
@@ -32,19 +22,6 @@ let k=0
 type=ukb
 gemma=/net/mulan/home/yasheng/comparisonProject/program/gemma-0.98.1-linux-static
 
-#for pc in 0.001 0.01 1 0.1 0.1; do
-#  let pc_ctr=${pc_ctr}+1
-#    hsq=0.2
-#    if [ ${pc_ctr} -eq 4 ]; then
-#      hsq=0.1
-#    fi
-#    if [ ${pc_ctr} -eq 5 ]; then
-#      hsq=0.5
-#    fi
-#pc=0.001
-#pc=0.1
-#hsq=0.2
-#hsq=0.5
 for p in `seq 1 10`
 do
 for fold in 1 2 3 4 5
