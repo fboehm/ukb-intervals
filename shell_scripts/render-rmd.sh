d8cdf0538027f31dbcdac37a37ec1db7d3d599a4
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-08-12 10:32:13 -0400

    added html results files

--- shell_scripts/render-rmd.sh
+++ shell_scripts/render-rmd.sh
@@ -4,7 +4,9 @@
 #SBATCH --time=3-00:00:00
 #SBATCH --job-name=cvplus
 #SBATCH --mem=16G
-#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%a.out
-#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%a.err
+#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%j_%a.out
+#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%j_%a.err
 
-Rscript -e 'rmarkdown::render("~/research/ukb-intervals/Rmd/cv-plus-simulations.Rmd")'
+Rscript -e 'rmarkdown::render(input = "~/research/ukb-intervals/Rmd/cv-plus-simulations.Rmd",
+                              output_file = "~/research/ukb-intervals/Rmd/cv-plus-simulations-alpha0.1-hsq0.2-pcausal0.1",
+                              params = list(hsq=0.2, pcausal=0.1))'
