36655965b287bbd7d2f3d406499a8a4a6c046ecc
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-10-17 14:13:12 -0400

    fixed path to herit file

--- code_sheng/06_DBSLMM_ukb.sh
+++ code_sheng/06_DBSLMM_ukb.sh
@@ -51,7 +51,7 @@ herit=${compstr}05_internal_c/pheno${p}/herit/h2_ukb_cross${cross}.log
 summ=${compstr}05_internal_c/pheno${p}/output/summary_ukb_cross${cross}_chr
 outPath=${fbstr}/05_internal_c/pheno${p}/DBSLMM/
 else
-herit=${compstr}06_internal_b/pheno${p}/herit/h2_ukb_cross${cross}.log
+herit=${fbstr}06_internal_b/pheno${p}/herit/h2_ukb_cross${cross}.log
 summ=${fbstr}06_internal_b/pheno${p}/output/summary_ukb_cross${cross}_chr
 outPath=${fbstr}/06_internal_b/pheno${p}/DBSLMM/
 # covariates for validation set
