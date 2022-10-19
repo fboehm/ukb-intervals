4e6d3439b16ddbda32d6a382a00af6c0cc1d32c1
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-10-19 09:57:32 -0400

    updated shell script for rendering rmd

--- shell_scripts/render-rmd.sh
+++ shell_scripts/render-rmd.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=3-00:00:00
 #SBATCH --job-name=cvplus
 #SBATCH --mem=16G
-#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%a.out
-#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%a.err
+#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd.out
+#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd.err
 
 Rscript -e 'rmarkdown::render(input = "~/research/ukb-intervals/Rmd/cv-plus.Rmd")'
