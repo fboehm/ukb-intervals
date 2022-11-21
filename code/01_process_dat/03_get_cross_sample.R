877241e873910f649bdbf7c9ee697b5fc8fbfa6a
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-11-21 10:41:25 -0500

    Fixed R scripts for making fam files

--- code/01_process_dat/03_get_cross_sample.R
+++ code/01_process_dat/03_get_cross_sample.R
@@ -74,10 +74,13 @@ for (p in 1:25) {
   pheno_c_adj_val <- pheno_c_adj[match(idx_val, sqc_i$idx), p]
   # ## output
   write.table(cbind(idx_val, idx_val),
-               file = paste0(comp_str, "03_subsample/continuous/pheno", p,
-                             "/val/01_idx.txt"),
-               col.names = F, row.names = F, quote = F)
-  
+              file = paste0(comp_str, 
+                            "03_subsample/continuous/pheno", 
+                            p,
+                            "/val/01_idx.txt"),
+               col.names = F, 
+               row.names = F, 
+               quote = F)
   # Step2: cross validation set
   idx_cv <- setdiff(idx_tot, idx_val)
   set.seed(pheno_seed_str[p]+cross_seed)
@@ -89,17 +92,19 @@ for (p in 1:25) {
                                     )
 #  group_test <- sample(c(2: group_num), fold, replace = F) ## for
   pheno_train <- pheno_test <- matrix(NA, nrow(pheno_c_adj), 5)
-  for(cross in 1: n_folds) {
+  for (cross in 1: n_folds) {
     ## test set
-    #idx_test <- idx_group[[group_test[cross]]][, 1]
     # get indices for each test fold
     idx_test <- idx_cv[split_indices[[cross]]]
-    # 
+    # column of pheno_test is initialized with column from pheno_c_adj
     pheno_test[, cross] <- pheno_c_adj[, p]
-    pheno_test[!sqc_i$idx%in%idx_test, cross] <- NA
+    # same column has non-test entries set to NA
+    pheno_test[!sqc_i$idx %in% idx_test, cross] <- NA
     ## train set
     idx_train <- setdiff(idx_cv, idx_test)
+    # initially, set pheno_train column to the full trait vector
     pheno_train[, cross] <- pheno_c_adj[, p]
+    # then, replace test subjects' entries with NA
     pheno_train[!sqc_i$idx %in% idx_train, cross] <- NA
     write.table(cbind(idx_test, idx_test),
                 file = paste0(comp_str, 
@@ -112,53 +117,53 @@ for (p in 1:25) {
                 row.names = F, 
                 col.names = F)
   }
-  # write.table(pheno_train, file = paste0(comp_str, "02_pheno/02_train_c/pheno_pheno", p, ".txt"),
-  #             quote = F, row.names = F, col.names = F)
-  # write.table(pheno_test, file = paste0(comp_str, "02_pheno/03_test_c/pheno_pheno", p, ".txt"),
-  #             quote = F, row.names = F, col.names = F)
-  
-  sample_size_dat <- rbind(sample_size_dat, sample_size_mat)
+  write.table(pheno_train, 
+              file = paste0(comp_str,
+                            "02_pheno/02_train_c/pheno_pheno", 
+                            p,
+                            ".txt"),
+              quote = F, 
+              row.names = F, 
+              col.names = F
+              )
+  write.table(pheno_test, 
+              file = paste0(comp_str, 
+                            "02_pheno/03_test_c/pheno_pheno", 
+                            p, 
+                            ".txt"),
+              quote = F, 
+              row.names = F, 
+              col.names = F
+              )
 }
 pheno_uni_c <- c("SH", "PLT", "BMD", "BMR", "BMI", 
                  "RBC", "AM", "RDW", "EOS", "WBC", 
                  "FVC", "FEV", "FFR", "WC", "HC",
                  "WHR", "SBP", "BW", "BFP", "TFP", 
                  "SU", "TC", "HDL", "LDL", "TG")
-sample_size_dat <- data.frame(rep(pheno_uni_c, each = 5), 
-                              sample_size_dat)
-write.table(sample_size_dat, file = paste0(comp_str, "02_pheno/sample_size_c.txt"),
-            quote = F, row.names = F, col.names = F)
 
 
 
 ##########################
 # Binary trait
-sample_size_dat <- data.frame()
-covVar_all <- as.matrix(cbind(sqc_i[, c(26:35)], 
+covVar_all <- as.matrix(cbind(sqc_i[, c(26:35)], # first ten PCs
                               ifelse(sqc_i$Inferred.Gender == "F", 0, 1)))
 for (p in 1:25) {
-  sample_size_mat <- matrix(NA, fold, 5)
   idx_tot <- setdiff(sqc_sub$idx, na_list_b[[p]])
-  sample_size_mat[c(1:5), 1] <- length(idx_tot)
-  cat("pheno", p, "include", length(idx_tot), "samples.\n")
+  cat("pheno", p, " includes ", length(idx_tot), " samples.\n")
   set.seed(pheno_seed_str[p])
-  idx_group <- idx_tot %>% as.data.frame() %>% 
-    split(sample(1:group_num, length(idx_tot), replace=T))
+  ##idx_group <- idx_tot %>% 
+  #                as.data.frame() %>% 
+  #                split(sample(1:group_num, length(idx_tot), replace=T))
   
   # Step1: validation and PGSagg set
   ## get index
   set.seed(pheno_seed_str[p])
-  idx_val_agg <- idx_group[[1]] %>% 
-    split(sample(1:2, nrow(idx_group[[1]]), replace=T))
-  idx_val <- idx_val_agg[[1]][, 1]
-  idx_agg <- idx_val_agg[[2]][, 1]
-  sample_size_mat[c(1:5), 2] <- length(idx_val)
-  sample_size_mat[c(1:5), 3] <- length(idx_agg)
-  cat("pheno", p, "include val", length(idx_val), "samples, and", 
-      "agg", length(idx_agg), "samples.\n")
+    idx_val <- sample(x = idx_tot, size = floor(0.1 * length(idx_tot)), replace = FALSE)
+
+  cat("pheno", p, "include val ", length(idx_val), "samples.\n")
   ## pheno data
   pheno_b_all_val <- pheno_b_all[match(idx_val, sqc_i$idx), p]
-  pheno_b_all_agg <- pheno_b_all[match(idx_agg, sqc_i$idx), p]
   ## covariates data
   if(!p %in% c(1, 6, 21)){
     ## validation
@@ -171,16 +176,6 @@ for (p in 1:25) {
     coefMat_val_glm <- glm(y~., data = covVar_val,
                            family = binomial(link = "logit")) %>% coef()
     pred_val_glm <- cbind(1, covVar_all[match(idx_val, sqc_i$idx), ]) %*% coefMat_val_glm
-    ## aggregation
-    covVar_agg <- data.frame(y = pheno_b_all_agg, 
-                             covVar_all[match(idx_agg, sqc_i$idx), ])
-    # linear model
-    coefMat_agg <- lm(y~., data = covVar_agg) %>% coef()
-    pred_agg <- cbind(1, covVar_all[match(idx_agg, sqc_i$idx), ]) %*% coefMat_agg
-    # logistic regression
-    coefMat_agg_glm <- glm(y~., data = covVar_agg,
-                           family = binomial(link = "logit")) %>% coef()
-    pred_agg_glm <- cbind(1, covVar_all[match(idx_agg, sqc_i$idx), ]) %*% coefMat_agg_glm
     
   } else {
     ## validation
@@ -194,16 +189,6 @@ for (p in 1:25) {
                            family = binomial(link = "logit")) %>% coef()
     pred_val_glm <- cbind(1, covVar_all[match(idx_val, sqc_i$idx), -11]) %*% coefMat_val_glm
     
-    ## aggregation
-    covVar_agg <- data.frame(y = pheno_b_all_agg, 
-                             covVar_all[match(idx_agg, sqc_i$idx), -11])
-    # linear model
-    coefMat_agg <- lm(y~., data = covVar_agg) %>% coef()
-    pred_agg <- cbind(1, covVar_all[match(idx_agg, sqc_i$idx), -11]) %*% coefMat_agg
-    # logistic regression
-    coefMat_agg_glm <- glm(y~., data = covVar_agg, 
-                           family = binomial(link = "logit")) %>% coef()
-    pred_agg_glm <- cbind(1, covVar_all[match(idx_agg, sqc_i$idx), -11]) %*% coefMat_agg_glm
   }
   ## output
    write.table(cbind(idx_val, idx_val), 
@@ -218,39 +203,25 @@ for (p in 1:25) {
                file = paste0(comp_str, "03_subsample/binary/pheno",p,
                              "/val/03_cov_eff.txt"), 
                row.names = F, col.names = F, quote = F)
-  # write.table(pred_val_glm,
-  #             file = paste0(comp_str, "03_subsample/binary/pheno",p,
-  #                           "/val/04_cov_eff_glm.txt"),
-  #             row.names = F, col.names = F, quote = F)
-  # write.table(cbind(idx_agg, idx_agg), 
-  #             file = paste0(comp_str, "03_subsample/binary/pheno", p,
-  #                           "/agg/01_idx.txt"),
-  #             col.names = F, row.names = F, quote = F)
-  # write.table(pheno_b_all_agg, 
-  #             file = paste0(comp_str, "03_subsample/binary/pheno", p,
-  #                           "/agg/02_pheno_b.txt"), 
-  #             col.names = F, row.names = F, quote = F)
-  # write.table(pred_agg, 
-  #             file = paste0(comp_str, "03_subsample/binary/pheno",p,
-  #                           "/agg/03_cov_eff.txt"), 
-  #             row.names = F, col.names = F, quote = F)
-  # write.table(pred_agg_glm,
-  #             file = paste0(comp_str, "03_subsample/binary/pheno",p,
-  #                           "/agg/04_cov_eff_glm.txt"),
-  #             row.names = F, col.names = F, quote = F)
-  
-  
   # Step2: cross validation set
+  idx_cv <- setdiff(idx_tot, idx_val)
   set.seed(pheno_seed_str[p]+cross_seed)
-  group_test <- sample(c(2: group_num), fold, replace = F) ## for
+  split_indices <- splitTools::partition(idx_cv, p = c(fold1 = 0.2, 
+                                    fold2 = 0.2, 
+                                    fold3 = 0.2, 
+                                    fold4 = 0.2, 
+                                    fold5 = 0.2)
+                                    )
+  #group_test <- sample(c(2: group_num), fold, replace = F) ## for
+  
   pheno_train <- pheno_test <- coveff <- coveff_glm <- matrix(NA, nrow(pheno_b_all), 5)
-  for(cross in 1: fold){
+  for(cross in 1:n_folds){
+    idx_test <- idx_cv[split_indices[[cross]]]
+    idx_train <- setdiff(idx_cv, idx_test)
     
     ## train set
-    idx_train <- idx_group[-c(1, group_test[cross])] %>% llply(function(a) a[, 1]) %>%
-      unlist %>% sort
     pheno_train[, cross] <- pheno_b_all[, p]
-    pheno_train[!sqc_i$idx%in%idx_train, cross] <- NA
+    pheno_train[!sqc_i$idx %in% idx_train, cross] <- NA
     if(!p %in% c(1, 6, 21)){
       covVar_train <- data.frame(y = pheno_train[, cross], 
                                  covVar_all)
@@ -272,21 +243,16 @@ for (p in 1:25) {
     }
     
     ## test set
-    idx_test <- idx_group[[group_test[cross]]][, 1]
     pheno_test[, cross] <- pheno_b_all[, p]
-    pheno_test[!sqc_i$idx%in%idx_test, cross] <- NA
+    pheno_test[!sqc_i$idx %in% idx_test, cross] <- NA
     if(!p %in% c(1, 6, 21)){
-      coveff[sqc_i$idx%in%idx_test, cross] <- cbind(1, covVar_all[match(idx_test, sqc_i$idx), ]) %*% coefMat_train
-      coveff_glm[sqc_i$idx%in%idx_test, cross] <- cbind(1, covVar_all[match(idx_test, sqc_i$idx), ]) %*% coefMat_train_glm
+      coveff[sqc_i$idx %in% idx_test, cross] <- cbind(1, covVar_all[match(idx_test, sqc_i$idx), ]) %*% coefMat_train
+      coveff_glm[sqc_i$idx %in% idx_test, cross] <- cbind(1, covVar_all[match(idx_test, sqc_i$idx), ]) %*% coefMat_train_glm
     } else {
-      coveff[sqc_i$idx%in%idx_test, cross] <- cbind(1, covVar_all[match(idx_test, sqc_i$idx), -11]) %*% coefMat_train
-      coveff_glm[sqc_i$idx%in%idx_test, cross] <- cbind(1, covVar_all[match(idx_test, sqc_i$idx), -11]) %*% coefMat_train_glm
+      coveff[sqc_i$idx %in% idx_test, cross] <- cbind(1, covVar_all[match(idx_test, sqc_i$idx), -11]) %*% coefMat_train
+      coveff_glm[sqc_i$idx %in% idx_test, cross] <- cbind(1, covVar_all[match(idx_test, sqc_i$idx), -11]) %*% coefMat_train_glm
     }
-    sample_size_mat[cross, 4] <- length(idx_test)
-    sample_size_mat[cross, 5] <- sum(!is.na(pheno_train[, cross]))
-    cat("pheno", p, "include train", sample_size_mat[cross, 5],
-        "samples, and test", sample_size_mat[cross, 4], "samples.\n")
-     write.table(cbind(idx_test, idx_test),
+    write.table(cbind(idx_test, idx_test),
                 file = paste0(comp_str, "02_pheno/04_test_idx_b/idx_pheno", p, "_cross", cross, ".txt"),
                 quote = F, row.names = F, col.names = F)
   }
@@ -298,14 +264,10 @@ for (p in 1:25) {
                quote = F, row.names = F, col.names = F)
    write.table(coveff_glm, file = paste0(comp_str, "02_pheno/06_test_b/coveff_pheno", p, "_glm.txt"),
                quote = F, row.names = F, col.names = F)
-  sample_size_dat <- rbind(sample_size_dat, sample_size_mat)
 }
 pheno_uni_b <- c("PRCA", "TA", "T2D", "CAD", "RA", 
                  "BRCA", "AS", "MP", "MDD", "SS", 
                  "QU", "HT", "FFI", "DFI", "OA", 
                  "AN", "GO", "SAF", "HA", "TE", 
                  "T1B", "VMS", "MY", "SN", "ES")
-sample_size_dat <- data.frame(rep(pheno_uni_b, each = 5), sample_size_dat)
-write.table(sample_size_dat, file = paste0(comp_str, "02_pheno/xsample_size_b.txt"),
-            quote = F, row.names = F, col.names = F)
 
--- code/01_process_dat/08_write_pheno.R
+++ code/01_process_dat/08_write_pheno.R
@@ -2,30 +2,36 @@
 rm(list=ls())
 library(parallel)
 library(bigreadr)
-comp_str <- "/net/mulan/disk2/yasheng/comparisonProject/"
-pred_str <- "/net/mulan/home/yasheng/predictionProject/"
+trait_type <- "binary"
+comp_str <- "~/research/ukb-intervals/"
+pred_str <- "/net/mulan/disk2/yasheng/predictionProject/"
 ## output to fam file for cross validation 
 pheno_num <- 25
 pheno <- matrix(NA, nrow = 337129, ncol = pheno_num*5)
 for (p in 1: pheno_num){
   begin <- (p-1)*5+1
   end <- (p-1)*5+5
-  pheno_str <- paste0(comp_str, "02_pheno/02_train_c/pheno_pheno", p, ".txt")
-  # pheno_str <- paste0(comp_str, "02_pheno/05_train_b/pheno_pheno", p, ".txt")
+  if (trait_type == "continuous") {
+    pheno_str <- paste0(comp_str, "02_pheno/02_train_c/pheno_pheno", p, ".txt")
+  } else {
+    pheno_str <- paste0(comp_str, "02_pheno/05_train_b/pheno_pheno", p, ".txt")
+  }
   pheno[, c(begin:end)] <- as.matrix(fread2(pheno_str))
 }
-fam <- fread2(paste0(pred_str, "plink_file/ukb/chr22.fam"))
+fam <- readr::read_table(paste0(pred_str, "plink_file/ukb/chr22.fam"), col_names = FALSE)
 # example
 ####### return 1
 # fam_pheno <- cbind(fam[, c(1:5)], -9)
 #######
 fam_pheno <- cbind(fam[, c(1:5)], pheno) # pheno
-fam_output <- mclapply(c(1:22), function(chr){
-  write.table(fam_pheno, file = paste0(pred_str, "plink_file/ukb/chr", chr, ".fam"),
-              col.names = F, row.names = F, quote = F)
-  # write.table(fam_pheno, file = paste0(pred_str, "plink_file/hm3/chr", chr, ".fam"),
-  #             col.names = F, row.names = F, quote = F)
-  # write.table(fam_pheno, file = paste0(pred_str, "plink_file/rare/chr", chr, ".fam"),
-  #             col.names = F, row.names = F, quote = F)
-  return(chr)
-},  mc.cores=22)
+write.table(fam_pheno, 
+              file = paste0(comp_str, 
+                            "plink_file/ukb/", 
+                            trait_type, 
+                            "/chr", 
+                            1, 
+                            ".fam"),
+              col.names = F, 
+              row.names = F, 
+              quote = F
+              )
