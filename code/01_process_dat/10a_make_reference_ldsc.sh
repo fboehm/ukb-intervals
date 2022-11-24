4cf034101e5e5db9d76d6d5c450717f52e34b95b
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-11-24 16:44:58 -0500

    updated scripts with ldsc

--- code/01_process_dat/10a_make_reference_ldsc.sh
+++ code/01_process_dat/10a_make_reference_ldsc.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-#SBATCH --partition=mulan,main
-#SBATCH --time=1-00:00:00
-#SBATCH --job-name=ldsc_ref
-#SBATCH --mem=2G
-#SBATCH --cpus-per-task=1
-#SBATCH --array=1-44
-#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/10_ldsc_%a.out
-#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/10_ldsc_%a.err
 
 CONDA_BASE=$(conda info --base) 
 source $CONDA_BASE/etc/profile.d/conda.sh
 
 trait_types=(binary continuous)
 fbstr=~/research/ukb-intervals/
+#ldsc=/usr/cluster/ldsc/ldsc.py
 ldsc=~/ldsc/ldsc.py
 
-let k=0
 
 for trait_type in ${trait_types[@]}; do
 	outpath=${fbstr}04_reference/ukb/${trait_type}/ldsc/
 	mkdir -p ${outpath}
-	for chr in `seq 1 22`;do
-		let k=${k}+1
-		if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
+	for chr in `seq 2 22`;do
 			conda activate ldsc2
 			python2 ${ldsc} --out ${outpath}${chr} \
 					--bfile ${fbstr}04_reference/ukb/${trait_type}/geno/chr${chr} \
 					--l2 \
 					--ld-wind-kb 1000.0 
-		fi
 	done
 done
--- code/02_method/04_herit.sh
+++ code/02_method/04_herit.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
 
-#SBATCH --partition=mulan,nomosix
-#SBATCH --time=03:00:00
+#SBATCH --partition=mulan,main
+#SBATCH --time=20:00:00
 #SBATCH --job-name=herit
 #SBATCH --mem-per-cpu=10G
+#SBATCH --array=1-10
+#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/04_herit_%a.out
+#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/04_herit_%a.err
 
-#SBATCH --array=1-125
-#SBATCH --output=/net/mulan/disk2/yasheng/comparisonProject/00_cluster_file/04_herit_%a.out
-#SBATCH --error=/net/mulan/disk2/yasheng/comparisonProject/00_cluster_file/04_herit_%a.err
 
-bash
+trait_types=(binary continuous)
+compStr=~/research/ukb-intervals/
+
 let k=0
 
-ldsc=/net/mulan/home/yasheng/comparisonProject/program/ldsc/ldsc.py
-mkldsc=/net/mulan/home/yasheng/comparisonProject/code/02_method/04_mk_ldsc_summ.R
-compstr=/net/mulan/disk2/yasheng/comparisonProject/
-dat=c
-reftype=ukb
+#ldsc=/usr/cluster/ldsc/ldsc.py
+ldsc=~/ldsc/ldsc.py
+mkldsc=${compStr}code/02_method/04_mk_ldsc_summ.R
 
-for p in `seq 1 25`; do
-for cross in 1 2 3 4 5; do
-let k=${k}+1
-if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
-
-ref=/net/mulan/disk2/yasheng/comparisonProject/04_reference/${reftype}/ldsc/
-
-if [[ "$dat" == "c" ]]
-then
-summ=${compstr}05_internal_c/pheno${p}/output/summary_${reftype}_cross${cross}
-h2=${compstr}05_internal_c/pheno${p}/herit/h2_${reftype}_cross${cross}
-else
-summ=${compstr}06_internal_b/pheno${p}/output/summary_hm3_cross${cross}
-h2=${compstr}06_internal_b/pheno${p}/herit/h2_${reftype}_cross${cross}
-fi
-
-## summary data for ldsc
-cat ${summ}_chr*.assoc.txt > ${summ}.assoc.txt
-sed -i '/chr/d' ${summ}.assoc.txt
-Rscript ${mkldsc} --summgemma ${summ}.assoc.txt --summldsc ${summ}.ldsc
-
-## heritability
-source activate /net/mulan/home/yasheng/py3/envs/ldsc
-python2 ${ldsc} --h2 ${summ}.ldsc.gz --ref-ld-chr ${ref} --w-ld-chr ${ref} --out ${h2}
-rm ${summ}.ldsc.gz
-fi
-done
+for trait_type in ${trait_types[@]}; do
+    #for p in `seq 1 25`; do
+    for p in 1; do 
+        for cross in 1 2 3 4 5; do
+            let k=${k}+1
+            if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
+                ref=${compStr}04_reference/ukb/${trait_type}/ldsc/
+                if [[ "${trait_type}" == "continuous" ]]; then
+                    summ=${compStr}05_internal_c/pheno${p}/output/summary_ukb_cross${cross}
+                    h2path=${compStr}05_internal_c/pheno${p}/herit/
+                    h2=${h2path}h2_ukb_cross${cross}
+                else
+                    summ=${compStr}06_internal_b/pheno${p}/output/summary_ukb_cross${cross}
+                    h2path=${compStr}06_internal_b/pheno${p}/herit/
+                    h2=${h2path}h2_ukb_cross${cross}
+                fi
+                mkdir -p ${h2path}
+                ## summary data for ldsc
+                cat ${summ}_chr*.assoc.txt > ${summ}.assoc.txt
+                sed -i '/chr/d' ${summ}.assoc.txt
+                Rscript ${mkldsc} --summgemma ${summ}.assoc.txt --summldsc ${summ}.ldsc
+                ## heritability
+                conda activate ldsc2
+                python2 ${ldsc} --h2 ${summ}.ldsc.gz --ref-ld-chr ${ref} --w-ld-chr ${ref} --out ${h2}
+                rm ${summ}.ldsc.gz
+            fi
+        done
+    done
 done
