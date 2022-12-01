d4b69a9678814aff6fe05412adc36c8449bb6d3a
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-12-01 10:20:29 -0500

    corrected pheno numbers

--- code/01_process_dat/09_make_summary.sh
+++ code/01_process_dat/09_make_summary.sh
@@ -19,43 +19,49 @@ dats=(1 2)
 type=ukb
 
 for dat in ${dats[@]}; do
-#for p in `seq 1 25`; do
-for p in 1; do
-  for cross in 1 2 3 4 5; do
-    for chr in `seq 1 22`; do
-      let k=${k}+1
-      if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
+  if [ ${dat} -eq 1 ]; then
+    let p_num=14; # waist circumference, WC. Sheng's code confirms that this is trait 14
+  fi
+  if [ ${dat} -eq 2 ]; then
+    let p_num=24; #snoring, SN
+  fi
+  #for p in `seq 1 25`; do
+  for p in ${p_num}; do
+    for cross in 1 2 3 4 5; do
+      for chr in `seq 1 22`; do
+        let k=${k}+1
+        if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
 
-      let col=(${p}-1)*5+${cross}
+        let col=(${p}-1)*5+${cross}
 
-      summ=summary_${type}_cross${cross}_chr${chr}
+        summ=summary_${type}_cross${cross}_chr${chr}
 
-      if [ ${dat} -eq 1 ]; then
-        echo continuous phenotype
-        bfile=${fbstr}plink_file/ukb/continuous/chr${chr}
-        cd ${fbstr}05_internal_c/pheno${p}
-        ${gemma} -bfile ${bfile} -notsnp -lm 1 -n ${col} -o ${summ}
-        sed -i '1d' ${fbstr}05_internal_c/pheno${p}/output/${summ}.assoc.txt
-        rm ${fbstr}05_internal_c/pheno${p}/output/${summ}.log.txt
-      else
-
-        echo binary phenotype
-        if ((${p}==1 || ${p}==6 || ${p}==21)); then
-          cov=${fbstr}02_pheno/08_cov_nosex.txt
+        if [ ${dat} -eq 1 ]; then
+          echo continuous phenotype
+          bfile=${fbstr}plink_file/ukb/continuous/chr${chr}
+          cd ${fbstr}05_internal_c/pheno${p}
+          ${gemma} -bfile ${bfile} -notsnp -lm 1 -n ${col} -o ${summ}
+          sed -i '1d' ${fbstr}05_internal_c/pheno${p}/output/${summ}.assoc.txt
+          rm ${fbstr}05_internal_c/pheno${p}/output/${summ}.log.txt
         else
-          cov=${fbstr}02_pheno/07_cov.txt
+
+          echo binary phenotype
+          if ((${p}==1 || ${p}==6 || ${p}==21)); then
+            cov=${fbstr}02_pheno/08_cov_nosex.txt
+          else
+            cov=${fbstr}02_pheno/07_cov.txt
+          fi
+          bfile=${fbstr}plink_file/ukb/binary/chr${chr}
+          cd ${fbstr}06_internal_b/pheno${p}
+          ${gemma} -bfile ${bfile} -notsnp -lm 1 -n ${col} -o ${summ} -c ${cov}
+          sed -i '1d' ${fbstr}06_internal_b/pheno${p}/output/${summ}.assoc.txt
+          rm ${fbstr}06_internal_b/pheno${p}/output/${summ}.log.txt
+          
+          fi
+          
         fi
-        bfile=${fbstr}plink_file/ukb/binary/chr${chr}
-        cd ${fbstr}06_internal_b/pheno${p}
-        ${gemma} -bfile ${bfile} -notsnp -lm 1 -n ${col} -o ${summ} -c ${cov}
-        sed -i '1d' ${fbstr}06_internal_b/pheno${p}/output/${summ}.assoc.txt
-#          rm ${fbstr}06_internal_b/pheno${p}/output/${summ}.log.txt
-        
-        fi
-        
-      fi
-    
+      
+      done
     done
   done
-done
 done
