8e0c6c172a977b9f48342779545cc105670c9b80
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-12-24 14:31:45 -0500

    added shell script for making sym links to val geno files

--- study_nfolds/code/01_process_dat/05a_get_subsample.sh
+++ study_nfolds/code/01_process_dat/05a_get_subsample.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --partition=mulan,main
-#SBATCH --time=2-00:00:00
+#SBATCH --time=4-00:00:00
 #SBATCH --job-name=subset
 #SBATCH --mem=2G
 #SBATCH --cpus-per-task=1
-#SBATCH --array=1-3300%400
+#SBATCH --array=1-1100%300
 #SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/05a_subsample_%a.out
 #SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/05a_subsample_%a.err
 
@@ -14,40 +14,41 @@ trait_types=(binary continuous)
 nfolds=(5 10 20)
 
 let k=0
-
-for nfold in ${nfolds[@]}; do
-    compStr=~/research/ukb-intervals/study_nfolds/${nfold}-fold/
-    for trait_type in ${trait_types[@]}; do
-        for chr in `seq 1 22`;do
-            for p in `seq 1 25`; do
-                let k=${k}+1
-                if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
-                    bfileAll=${olddir}plink_file/ukb/${trait_type}/chr${chr}
-                    ## validation sample
-                    mydir=${compStr}03_subsample/${trait_type}/pheno${p}/val/
-                    bfileSub=${mydir}ukb/geno/allchr${chr}
-                    bfileSubP=${mydir}ukb/geno/chr${chr}
-                    mkdir -p ${mydir}ukb/geno
-                    idxSub=${mydir}01_idx.txt
-                    if [ ! -f "${bfileSubP}.bed" ]; then
-                        plink-1.9 --silent --bfile ${bfileAll} --keep ${idxSub} --make-bed --out ${bfileSub}
-                        plink-1.9 --silent --bfile ${bfileSub} --maf 0.01 --make-bed --out ${bfileSubP}
-                        rm ${bfileSub}.log
-                        rm ${bfileSub}.bed
-                        rm ${bfileSub}.bim
-                        rm ${bfileSub}.fam
-                        rm ${bfileSubP}.log
-                    fi
-                    # impute
-                    geno_impute=05_geno_imputation.R
-                    input=${bfileSubP}
-                    output=${mydir}impute/chr${chr}
-                    mkdir -p ${mydir}impute
-                    if [ ! -f "${output}.bed" ]; then    
-                        Rscript ${geno_impute} --plinkin ${input} --plinkout ${output}
-                    fi
+let nfold=5
+# every fold value has the same validation set, so 
+# only need to do subsetting for a single fold value, 
+# then sym link to results
+compStr=~/research/ukb-intervals/study_nfolds/${nfold}-fold/
+for trait_type in ${trait_types[@]}; do
+    for chr in `seq 1 22`;do
+        for p in `seq 1 25`; do
+            let k=${k}+1
+            if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
+                bfileAll=${olddir}plink_file/ukb/${trait_type}/chr${chr}
+                ## validation sample
+                mydir=${compStr}03_subsample/${trait_type}/pheno${p}/val/
+                bfileSub=${mydir}ukb/geno/allchr${chr}
+                bfileSubP=${mydir}ukb/geno/chr${chr}
+                mkdir -p ${mydir}ukb/geno
+                idxSub=${mydir}01_idx.txt
+                if [ ! -f "${bfileSubP}.bed" ]; then
+                    plink-1.9 --silent --bfile ${bfileAll} --keep ${idxSub} --make-bed --out ${bfileSub}
+                    plink-1.9 --silent --bfile ${bfileSub} --maf 0.01 --make-bed --out ${bfileSubP}
+                    rm ${bfileSub}.log
+                    rm ${bfileSub}.bed
+                    rm ${bfileSub}.bim
+                    rm ${bfileSub}.fam
+                    rm ${bfileSubP}.log
                 fi
-            done
+                # impute
+                geno_impute=05_geno_imputation.R
+                input=${bfileSubP}
+                output=${mydir}impute/chr${chr}
+                mkdir -p ${mydir}impute
+                if [ ! -f "${output}.bed" ]; then    
+                    Rscript ${geno_impute} --plinkin ${input} --plinkout ${output}
+                fi
+            fi
         done
     done
 done
---
+++ study_nfolds/code/01_process_dat/05b_symlinks.sh
@@ -0,0 +1,37 @@
+# we need to write sym links for the 
+# plink files created by 05a_get_subsample.sh
+
+trait_types=(binary continuous)
+nfolds=(10 20)
+
+let k=0
+# every fold value has the same validation set, so 
+# only need to do subsetting for a single fold value, 
+# then sym link to results
+for nfold in ${nfolds[@]}; do
+    compStr=~/research/ukb-intervals/study_nfolds/${nfold}-fold/
+    for trait_type in ${trait_types[@]}; do
+        for chr in `seq 1 22`;do
+            for p in `seq 1 25`; do
+                # remove old file if it's there
+                mydir=${compStr}03_subsample/${trait_type}/pheno${p}/val/
+                bfileSubP=${mydir}ukb/geno/chr${chr}
+                if [ -f "${bfileSubP}.bed" ]; then
+                    rm ${bfileSubP}.*
+                fi    
+                output=${mydir}impute/chr${chr}
+                if [ -f "${output}.bed" ]; then
+                    rm ${output}.*
+                fi
+                # make sym links
+                ln -s ~/research/ukb-intervals/study_nfolds/5-fold/03_subsample/${trait_type}/pheno${p}/val/ukb/geno/chr${chr}.bed ${bfileSubP}.bed
+                ln -s ~/research/ukb-intervals/study_nfolds/5-fold/03_subsample/${trait_type}/pheno${p}/val/ukb/geno/chr${chr}.bim ${bfileSubP}.bim
+                ln -s ~/research/ukb-intervals/study_nfolds/5-fold/03_subsample/${trait_type}/pheno${p}/val/ukb/geno/chr${chr}.fam ${bfileSubP}.fam
+                # imput sym links
+                ln -s ~/research/ukb-intervals/study_nfolds/5-fold/03_subsample/${trait_type}/pheno${p}/val/impute/chr${chr}.bed ${output}.bed
+                ln -s ~/research/ukb-intervals/study_nfolds/5-fold/03_subsample/${trait_type}/pheno${p}/val/impute/chr${chr}.bim ${output}.bim
+                ln -s ~/research/ukb-intervals/study_nfolds/5-fold/03_subsample/${trait_type}/pheno${p}/val/impute/chr${chr}.fam ${output}.fam
+            done
+        done
+    done
+done
