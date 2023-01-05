83347bcbf3b66a6ac34136521f1225deb3af9143
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-01-04 20:50:25 -0500

    fixed issue with output_dir

--- study_nfolds/code/02_method/call_Rmarkdown.R
+++ study_nfolds/code/02_method/call_Rmarkdown.R
@@ -4,9 +4,10 @@ aa <- commandArgs(trailingOnly = TRUE)
 (trait_number <- as.numeric(aa[3]))
 (output_dir <- aa[4])
 
-rmarkdown::render(input = "cv-plus.Rmd", output_dir = output_dir, 
-                params = list(trait_type = trait_type, nfold = nfold, trait_number = trait_number))
-                
+rmarkdown::render(input = "~/research/ukb-intervals/study_nfolds/code/02_method/cv-plus.Rmd", 
+                    output_dir = output_dir,
+                    params = list(trait_type = trait_type, nfold = nfold, trait_number = trait_number))
+
 
 
 
--- study_nfolds/code/02_method/render-rmd.sh
+++ study_nfolds/code/02_method/render-rmd.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=10:00:00
 #SBATCH --job-name=cvplus
 #SBATCH --mem=4G
-#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%a.out
-#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/render-rmd_%a.err
+#SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/rmd2_%a.out
+#SBATCH --error=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/rmd2_%a.err
 #SBATCH --array=1-150
 
 
@@ -18,12 +18,14 @@ for nfold in ${nfolds[@]}; do
     for dat in ${dats[@]}; do
         for p in `seq 1 25`; do
             let k=${k}+1
-            if [ "${dat}"=="continuous" ]; then
+            if [[ "${dat}" == "continuous" ]]; then
                 output_dir=~/research/ukb-intervals/study_nfolds/${nfold}-fold/05_internal_c/pheno${p}/cvplus/
             else 
                 output_dir=~/research/ukb-intervals/study_nfolds/${nfold}-fold/06_internal_b/pheno${p}/cvplus/
             fi
+            
             if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
+                echo "output directory is ${output_dir}"
                 R -e 'source("~/research/ukb-intervals/study_nfolds/code/02_method/call_Rmarkdown.R")' \
                     --args ${dat} ${nfold} ${p} ${output_dir}
             fi
